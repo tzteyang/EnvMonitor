52a6516cc0fc6ce687d51b9d3861be38c2d6848c
Asriel <1134175939@qq.com>
2023-02-16 10:07:04 +0800

    Initial commit

---
+++ src/main/java/com/envmonitor/EnvMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.envmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/envmonitor/EnvMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
